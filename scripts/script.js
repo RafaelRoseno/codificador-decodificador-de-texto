db4495388afce2af4381b7b351b93fa3ec363eb1
Rafael Roseno <rafaelroseno@outlook.com>
2024-01-30 16:08:57 -0300

    Adicionando a funcionalidade de descriptografar

--- scripts/script.js
+++ scripts/script.js
@@ -1,24 +1,41 @@
-var encryptReferences = [
+const encryptReferences = [
     [/a/g, /e/g, /i/g, /o/g, /u/g],
     ['ai', 'enter', 'imes', 'ober', 'ufat']
 ];
 
+const decryptReferences = [
+    [/ufat/g, /ober/g, /imes/g, /enter/g, /ai/g],
+    ['u', 'o', 'i', 'e', 'a']
+];
+
 function processEncrypt(){
-    let textToEncrypt = document.querySelector('.main-container__input-area__textarea');
+    const textToEncrypt = getTextAreaInput();
+    const encrypted = replaceAllExpressions(textToEncrypt.value, encryptReferences);
+    outputProcess(encrypted);
+}
+
+function processDecrypt() {
+    const textToDecrypt = getTextAreaInput();
+    const decrypted = replaceAllExpressions(textToDecrypt.value, decryptReferences);
+    outputProcess(decrypted);
+}
+
+function outputProcess(result) {
     let displayResult = document.querySelector('.main-container__display-area__result');
-    displayResult.innerHTML = encrypt(textToEncrypt.value);
-    textToEncrypt.value = '';
+    displayResult.textContent = result;
+    textAreaContent = getTextAreaInput();
+    textAreaContent.value = '';
 }
 
-function encrypt(textToEncrypt){
-    for (let column=0; column<encryptReferences[0].length; column++){
-        let expression = encryptReferences[0][column];
-        let value = encryptReferences[1][column];
-        textToEncrypt = textToEncrypt.replace(expression, value);
+function replaceAllExpressions(text, expressionList){
+    for (let column=0; column<expressionList[0].length; column++){
+        let expression = expressionList[0][column];
+        let value = expressionList[1][column];
+        text = text.replace(expression, value);
     }
-    return textToEncrypt;
+    return text;
 }
 
-function decrypt(){
-
+function getTextAreaInput() {
+    return document.querySelector('.main-container__input-area__textarea');
 }

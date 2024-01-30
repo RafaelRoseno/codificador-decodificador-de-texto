4042704c590fc6c9c003c5a90829a72e239d68f8
Rafael Roseno <rafaelroseno@outlook.com>
2024-01-30 18:15:06 -0300

    Adicionando comentarios descritivos no JavaScript

--- scripts/script.js
+++ scripts/script.js
@@ -1,32 +1,39 @@
-const encryptReferences = [
+const DISPLAY_AREA_SELECTOR = '.main-container__display-area__result';
+const TEXTAREA_SELECTOR = '.main-container__input-area__textarea';
+
+const ENCRYPTION_REFERENCES = [
     [/a/g, /e/g, /i/g, /o/g, /u/g],
     ['ai', 'enter', 'imes', 'ober', 'ufat']
 ];
 
-const decryptReferences = [
+const DECRYPTION_REFERENCES = [
     [/ufat/g, /ober/g, /imes/g, /enter/g, /ai/g],
     ['u', 'o', 'i', 'e', 'a']
 ];
 
+// Processa a criptografia do texto inserido pelo usuario
 function processEncrypt(){
-    const textToEncrypt = getTextAreaInput();
-    const encrypted = replaceAllExpressions(textToEncrypt.value, encryptReferences);
+    const textToEncrypt = getTextAreaContent();
+    const encrypted = replaceAllExpressions(textToEncrypt.value, ENCRYPTION_REFERENCES);
     outputProcess(encrypted);
 }
 
+// Processa a descriptografia do texto inserido pelo usuario
 function processDecrypt() {
-    const textToDecrypt = getTextAreaInput();
-    const decrypted = replaceAllExpressions(textToDecrypt.value, decryptReferences);
+    const textToDecrypt = getTextAreaContent();
+    const decrypted = replaceAllExpressions(textToDecrypt.value, DECRYPTION_REFERENCES);
     outputProcess(decrypted);
 }
 
+// Apresenta o resultado da criptografia/descriptografia na tela e aplica estilo na exibicao
 function outputProcess(result) {
-    let displayResult = document.querySelector('.main-container__display-area__result');
+    let displayResult = document.querySelector(DISPLAY_AREA_SELECTOR);
     displayResult.textContent = result;
-    textAreaContent = getTextAreaInput();
+    let textAreaContent = getTextAreaContent();
     textAreaContent.value = '';
 }
 
+// Itera uma lista de expressoes regulares e substitui seu valor em uma string
 function replaceAllExpressions(text, expressionList){
     for (let column=0; column<expressionList[0].length; column++){
         let expression = expressionList[0][column];
@@ -36,6 +43,6 @@ function replaceAllExpressions(text, expressionList){
     return text;
 }
 
-function getTextAreaInput() {
-    return document.querySelector('.main-container__input-area__textarea');
+function getTextAreaContent() {
+    return document.querySelector(TEXTAREA_SELECTOR);
 }

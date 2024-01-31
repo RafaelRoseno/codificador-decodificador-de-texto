7737da1f4598fb2ba75568924389d72b447db39a
Rafael Roseno <rafaelroseno@outlook.com>
2024-01-31 14:56:29 -0300

    Adicionando validação e mensagem de nenhum texto inserido

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,4 @@
+const NO_CONTENT_MESSAGE_SELECTOR = '.main-container__display-area__no-content';
 const DISPLAY_AREA_SELECTOR = '.main-container__display-area__result';
 const TEXTAREA_SELECTOR = '.main-container__input-area__textarea';
 
@@ -14,23 +15,24 @@ const DECRYPTION_REFERENCES = [
 // Processa a criptografia do texto inserido pelo usuario
 function processEncrypt(){
     const textToEncrypt = getTextAreaContent();
+    if (!textToEncrypt) return;
     const encrypted = replaceAllExpressions(textToEncrypt.value, ENCRYPTION_REFERENCES);
-    outputProcess(encrypted);
+    outputProcess(encrypted, textToEncrypt);
 }
 
 // Processa a descriptografia do texto inserido pelo usuario
 function processDecrypt() {
     const textToDecrypt = getTextAreaContent();
+    if (!textToDecrypt) return;
     const decrypted = replaceAllExpressions(textToDecrypt.value, DECRYPTION_REFERENCES);
-    outputProcess(decrypted);
+    outputProcess(decrypted, textToDecrypt);
 }
 
 // Apresenta o resultado da criptografia/descriptografia na tela com certa alteracao de estilo na exibicao
-function outputProcess(result) {
-    let displayResult = document.querySelector(DISPLAY_AREA_SELECTOR);
-    displayResult.textContent = result;
-    let textAreaContent = getTextAreaContent();
+function outputProcess(result, textAreaContent) {
+    showResult(result);
     textAreaContent.value = '';
+    hideNoContentMessage();
 }
 
 // Itera uma lista de expressoes regulares e substitui seu valor em uma string
@@ -44,5 +46,26 @@ function replaceAllExpressions(text, expressionList){
 }
 
 function getTextAreaContent() {
-    return document.querySelector(TEXTAREA_SELECTOR);
+    const inputText = document.querySelector(TEXTAREA_SELECTOR);
+    if (inputText.value.length == 0){
+        showResult('');
+        showNoContentMessage();
+        return null;
+    } 
+    return inputText;
+}
+
+function showResult(result) {
+    let displayResult = document.querySelector(DISPLAY_AREA_SELECTOR);
+    displayResult.textContent = result;
+}
+
+function hideNoContentMessage(){
+    let noContentDiv = document.querySelector(NO_CONTENT_MESSAGE_SELECTOR);
+    noContentDiv.style.visibility = 'hidden';
+}
+
+function showNoContentMessage(){
+    let noContentDiv = document.querySelector(NO_CONTENT_MESSAGE_SELECTOR);
+    noContentDiv.style.visibility = 'visible';
 }
